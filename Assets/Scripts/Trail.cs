00cd725563832d6cb4d530490189374e0880b699
tobray5 <tobray5@gmail.com>
2017-02-03 13:17:59 -0700

    Add Project

---
+++ Assets/Scripts/Trail.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Trail : MonoBehaviour {
+
+    private ParticleSystem ps;
+
+    public float lifetime = 0.1f;
+    public float lengthmodifier = 0.1f;
+
+    // Use this for initialization
+    void Start () {
+        ps = gameObject.GetComponent<ParticleSystem>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void  Upgrade() {
+        lifetime = lifetime + lengthmodifier;
+        ps.startLifetime = lifetime;
+    }
+}
